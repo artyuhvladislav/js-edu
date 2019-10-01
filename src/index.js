30f66ec58ea3cd31a575b353cbfd7c3bb256eb46
artyuhvladislav <workto22@gmail.com>
2019-10-01 14:41:08 +0300

    feat: add js-edu task

--- src/index.js
+++ src/index.js
@@ -4,11 +4,52 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+ module.exports = function getTimeForEducation(
+ 	focus = 'family', 
+ 	knowsProgramming = true,
+ 	config = {family: 4}
+ 	) {
+ 	if(knowsProgramming == true){ 
+ 		let time = 800;
+ 		if (focus =='family'){
+ 			return Math.ceil(time/config.family);
+ 		}
+ 		else if (focus =='friends'){
+ 			return Math.ceil(time/config.friends);
+ 		}
+ 		else if (focus =='normal_life'){
+ 			return Math.ceil(time/config.normal_life);
+ 		}
+ 		else if (focus =='profession'){
+ 			return Math.ceil(time/config.profession);
+ 		}
+ 		else if (focus =='carrier'){
+ 			return Math.ceil(time/config.carrier);
+ 		}
+ 		else if (focus =='top_peformance'){
+ 			return Math.ceil(time/config.top_peformance);
+ 		} 
+ 	}
+ 	else {
+ 		let time = 1300;
+ 		if (focus =='family'){
+ 			return Math.ceil(time/config.family);
+ 		}
+ 		else if (focus =='friends'){
+ 			return Math.ceil(time/config.friends);
+ 		}
+ 		else if (focus =='normal_life'){
+ 			return Math.ceil(time/config.normal_life);
+ 		}
+ 		else if (focus =='profession'){
+ 			return Math.ceil(time/config.profession);
+ 		}
+ 		else if (focus =='carrier'){
+ 			return Math.ceil(time/config.carrier);
+ 		}
+ 		else if (focus =='top_peformance'){
+ 			return Math.ceil(time/config.top_peformance);
+ 		}
+ 	}
+
+ };
